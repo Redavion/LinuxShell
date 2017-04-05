c3f2017413f7b3c22d8937950f9797b8f4714321
Redavion <alison.y.chen@vanderbilt.edu>
2017-04-05 10:17:06 -0500

    Add files via upload

---
+++ app.h
@@ -0,0 +1,28 @@
+#ifndef APP_H
+#define APP_H
+
+#include <iostream>
+#include <sched.h>
+#include <vector>
+#include <string>
+#include <map>
+
+class app
+{
+public:
+    app();
+    ~app();
+    void start();
+    static const std::vector< std::string > builtincmds;
+
+private:
+    // private methods
+    int execute( std::vector< std::string >& str );
+    int parallel_execution( std::string command_string );
+    int executebuiltin( std::vector< std::string >& command );
+
+    // private fields
+    int virtual_memory_limit;
+};
+
+#endif // APP_H
---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <memory>
+#include <vector>
+#include <string>
+
+#include "app.h"
+#include "utilities.h"
+
+int main( int argc, char* argv[] )
+{
+    std::auto_ptr< app > a( new app );
+    a->start();
+}
+
+void app::start()
+{
+    std::string command_str;
+    std::cerr << "\nvbash>>";
+    std::getline( std::cin, command_str );
+
+    // strip the command line of white space
+    // tokenize the commands into chunks of two
+    // With the "exit" command it exits the program
+    while ( command_str != "exit" )
+    {
+        command_str = trim( command_str );
+        if ( command_str.size() != 0 )
+        {
+            this->parallel_execution( command_str );
+        }
+        std::cerr << "\nvbash>>";
+        std::getline( std::cin, command_str );
+    }
+}
